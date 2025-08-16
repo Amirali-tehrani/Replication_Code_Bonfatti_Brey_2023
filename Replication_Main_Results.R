35805ca9f3f7f07f749b899a30099feec729aaae
LJN-max <lanceneidig@icloud.com>
2025-08-16 21:23:59 +0200

    Corrected Table 5

--- Replication_Main_Results.R
+++ Replication_Main_Results.R
@@ -12,15 +12,15 @@ library(modelsummary) # For making tables
 # Replication of Table 1 #
 ##########################
 
-# Read the data
-df <- read_dta("./Data/Analysis/Analysis_Data_Districtlevel.dta")
+# Load data
+table1 <- read_dta("./Data/Analysis/Analysis_Data_Districtlevel.dta")
 
 # Create list to store regression results
 models <- list()
 
 # Column 1
 models[["(1)"]] <- feols(D_Manufacturing ~ IM_Manu_Shock_17_13 + EX_Raw_Food_Shock_17_13,
-                         data = df, 
+                         data = table1, 
                          cluster = ~Census_Division)
 
 # Column 2
@@ -34,7 +34,7 @@ models[["(2)"]] <- feols(D_Manufacturing ~ IM_Manu_Shock_17_13 + EX_Raw_Food_Sho
                            Census_Dummy_13 + Census_Dummy_14 + Census_Dummy_15 +
                            Census_Dummy_16 + Census_Dummy_17 + Census_Dummy_18 +
                            Census_Dummy_19,
-                         data = df,
+                         data = table1,
                          cluster = ~Census_Division)
 
 # Column 3
@@ -48,7 +48,7 @@ models[["(3)"]] <- feols(D_Manufacturing ~ IM_Manu_Shock_17_13 + Manufacturing_1
                            Census_Dummy_13 + Census_Dummy_14 + Census_Dummy_15 +
                            Census_Dummy_16 + Census_Dummy_17 + Census_Dummy_18 +
                            Census_Dummy_19,
-                         data = df,
+                         data = table1,
                          cluster = ~Census_Division)
 
 # Column 4
@@ -62,11 +62,11 @@ models[["(4)"]] <- feols(D_Manufacturing ~ IM_Manu_Shock_17_13 + Manufacturing_1
                            Census_Dummy_13 + Census_Dummy_14 + Census_Dummy_15 +
                            Census_Dummy_16 + Census_Dummy_17 + Census_Dummy_18 +
                            Census_Dummy_19,
-                         data = df,
+                         data = table1,
                          cluster = ~Census_Division)
 
 # Column 5 (with subset condition)
-df_subset <- df %>%
+table1_subset <- table1 %>%
   filter(Native_State != "Native" | is.na(Native_State))
 
 models[["(5)"]] <- feols(D_Manufacturing ~ IM_Manu_Shock_17_13 + Manufacturing_1911 +
@@ -79,7 +79,7 @@ models[["(5)"]] <- feols(D_Manufacturing ~ IM_Manu_Shock_17_13 + Manufacturing_1
                            Census_Dummy_13 + Census_Dummy_14 + Census_Dummy_15 +
                            Census_Dummy_16 + Census_Dummy_17 + Census_Dummy_18 +
                            Census_Dummy_19,
-                         data = df_subset,
+                         data = table1_subset,
                          cluster = ~Census_Division)
 
 # Create custom gof_map for better formatting
@@ -122,4 +122,135 @@ modelsummary(models,
              threeparttable = TRUE,
              booktabs = TRUE)
 
+##########################
+# Replication of Table 2 #
+##########################
+
+# Columns 1-5: Industry-level
+
+# Load industry-level data
+industry_data <- read_dta("./Data/Analysis/Analysis_Data_Industrylevel.dta")
+
+# Column 1: Basic regression
+model1 <- feols(Perc_Manufacturing ~ D_IM_Manu_Trade_17_13 + D_EX_Raw_Food_Trade_17_13, 
+                data = industry_data, 
+                cluster = ~Sector2)
+
+# Column 2: Add Log_Manufacturing_1911
+model2 <- feols(Perc_Manufacturing ~ D_IM_Manu_Trade_17_13 + D_EX_Raw_Food_Trade_17_13 + 
+                  Log_Manufacturing_1911, 
+                data = industry_data, 
+                cluster = ~Sector2)
+
+# Column 3: Add sector fixed effects
+model3 <- feols(Perc_Manufacturing ~ D_IM_Manu_Trade_17_13 + D_EX_Raw_Food_Trade_17_13 + 
+                  Log_Manufacturing_1911 | Sector2, 
+                data = industry_data, 
+                cluster = ~Sector2)
+
+# Column 4: Subset to Manufactures == 1, exclude D_EX_Raw_Food_Trade_17_13
+model4 <- feols(Perc_Manufacturing ~ D_IM_Manu_Trade_17_13 + Log_Manufacturing_1911 | Sector2, 
+                data = industry_data, 
+                subset = ~Manufactures == 1,
+                cluster = ~Sector2)
+
+# Column 5: Add D_USA_Trade_21_13
+model5 <- feols(Perc_Manufacturing ~ D_IM_Manu_Trade_17_13 + D_USA_Trade_21_13 + 
+                  Log_Manufacturing_1911 | Sector2, 
+                data = industry_data, 
+                subset = ~Manufactures == 1,
+                cluster = ~Sector2)
+
+# Columns 6-7: Port-industry
+
+# Load port-level data
+port_data <- read_dta("Data/Analysis/Analysis_Data_Portlevel.dta") %>%
+  # Rename variable to match industry data
+  rename(D_IM_Manu_Trade_17_13 = IM_Manu_Shock_17_13)
+
+# Model 6: Port-level with three-digit industry fixed effects
+model6 <- feols(Perc_Manufacturing ~ D_IM_Manu_Trade_17_13 | ThreeDigit, 
+                data = port_data, 
+                subset = ~OneDigit == "Manufactures",
+                cluster = ~ThreeDigit)
+
+# Model 7: Port-level with four-digit industry and port fixed effects
+model7 <- feols(Perc_Manufacturing ~ D_IM_Manu_Trade_17_13 | Subdivision + Matchedcategory, 
+                data = port_data, 
+                subset = ~OneDigit == "Manufactures",
+                cluster = ~ThreeDigit)
+
+# Create list of models for table
+models <- list(
+  "(1)" = model1,
+  "(2)" = model2, 
+  "(3)" = model3,
+  "(4)" = model4,
+  "(5)" = model5,
+  "(6)" = model6,
+  "(7)" = model7
+)
+
+# Order of the variables
+coef_map <- c("D_IM_Manu_Trade_17_13" = "IM Manufactures Shock",
+              "D_EX_Raw_Food_Trade_17_13" = "EX Raw & Food Shock",
+              "D_USA_Trade_21_13" = "IM Shock (UK-USA)",
+              "Log_Manufacturing_1911" = "Log Employment 1911")
+
+# Display table in console
+modelsummary(models,
+             coef_map = coef_map,
+             stars = c('*' = .1, '**' = .05, '***' = .01),
+             gof_map = c("nobs", "r.squared"))
+
+# Export to LaTeX
+modelsummary(models,
+             output = "./Output/Table2.tex",
+             coef_map = coef_map,
+             stars = c('*' = .1, '**' = .05, '***' = .01),
+             fmt = 3,
+             gof_map = NA,
+             statistic = "std.error",
+             title = "Dependent variable: Industrial employment growth rate 1911–1921 (multiplied by 100)")
+
+##########################
+# Replication of Table 5 #
+##########################
+
+# Load data
+table5 <- read_dta("./Data/Analysis/Analysis_Data_CivilDisDistrictlevel.dta")
+
+# Drop observations where Native == 1
+table5 <- table5 %>% filter(Native != 1)
+
+# Create list to store models
+ivr <- list()
+
+# IV regreesion: Any_Response
+ivr[[1]] <- feols(Any_Response ~ Indian_Mutiny + Manufacturing_1911 + Military +
+                       Urban_1911 + Coastal + Literacy_Rate + Literacy_Eng + Age_above_20 + 
+                       Census_Dummy_1 + Census_Dummy_2 + Census_Dummy_3 + Census_Dummy_4 +
+                       Census_Dummy_5 + Census_Dummy_6 + Census_Dummy_7 + Census_Dummy_8 +
+                       Census_Dummy_9 + Census_Dummy_10 + Census_Dummy_11 + Census_Dummy_12 + 
+                       Census_Dummy_13 + Census_Dummy_14 + Census_Dummy_15 + Census_Dummy_16 + 
+                       Census_Dummy_17 + Census_Dummy_18 + Census_Dummy_19 |
+                       D_Manufacturing ~ IM_Manu_Shock_17_13,
+                     data = table5, vcov = "hetero")
+
+# IV regreesion: Dummy_KC
+ivr[[1]] <- feols(Any_Response ~ Indian_Mutiny + Manufacturing_1911 + Military +
+                    Urban_1911 + Coastal + Literacy_Rate + Literacy_Eng + Age_above_20 + 
+                    Census_Dummy_1 + Census_Dummy_2 + Census_Dummy_3 + Census_Dummy_4 +
+                    Census_Dummy_5 + Census_Dummy_6 + Census_Dummy_7 + Census_Dummy_8 +
+                    Census_Dummy_9 + Census_Dummy_10 + Census_Dummy_11 + Census_Dummy_12 + 
+                    Census_Dummy_13 + Census_Dummy_14 + Census_Dummy_15 + Census_Dummy_16 + 
+                    Census_Dummy_17 + Census_Dummy_18 + Census_Dummy_19 |
+                    D_Manufacturing ~ IM_Manu_Shock_17_13,
+                  data = table5, vcov = "hetero")
+
+
+
+##########################
+# Replication of Table 6 #
+##########################
 
